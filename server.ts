2b1439ed543eecbe33a0bbb3f37d9000f7f67527
liubin <bh0bama@sina.com>
2024-12-13 17:44:47 +0800

    update stream video

--- server.ts
+++ server.ts
@@ -1,14 +1,20 @@
 import Fastify from "fastify";
+import fs from "fs";
+import path from "path";
 
 const app = Fastify({
   logger: true,
 });
 
-app.get("/", function (req, reply) {
-  return { hello: "world" };
+app.get("/video", function (req, reply) {
+  const videoStream = fs.createReadStream(
+    path.join(__dirname, "assets", "video_uhd_2160_4096_30fps.mp4")
+  );
+  reply.send(videoStream);
 });
 
 app.listen({ host: "0.0.0.0", port: 9000 }, function (err, address) {
+  console.log("[*] Server starts at http://0.0.0.0:9000/");
   if (err) {
     app.log.error(err);
     process.exit(1);
